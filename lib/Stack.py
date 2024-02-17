5854898ae1bed2bba3517ef317fde24c2242ffbc
ErMuthii <er.muthii@gmail.com>
2024-02-17 21:28:57 +0300

    done

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,38 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = list(items)
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if self.isEmpty():
+            print('Stack is empty')
+        else:
+            return self.items.pop()
 
     def peek(self):
-        pass
+         if self.isEmpty():
+            print('Stack is empty')
+         else:
+            return self.items[-1]
+          
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
+
 
     def search(self, target):
-        pass
+        if target in self.items:
+            return len(self.items) - self.items.index(target) - 1
+        else:
+            return -1
